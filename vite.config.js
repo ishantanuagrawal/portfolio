a9f43b40ff044f22b223eae6a9c7f5eee16a2b15
ishantanuagrawal <ishantanuagrawal@gmail.com>
2026-02-21 02:05:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Replace 'portfolio' with your exact repo name
   plugins: [react()],
-  build: {
-    // This ensures modern JS features like import.meta.glob 
-    // are supported in the production build, fixing the 
-    // "import.meta is not available" warning.
-    target: 'esnext'
-  },
-  // This base path matches your GitLab repository: ishantanuagrawal-portfolio
-  // Note: If you point a custom domain (e.g., sbsmedia.in) to this site later,
-  // you will need to change this base back to '/'
-  base: '/', 
 })
