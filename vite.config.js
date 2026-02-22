2f73363cbc0165e625add6fbeca16f173046aac7
Shantanu Agrawal <shantanu@Shantanus-MacBook-Air.local>
2026-02-22 08:51:23 +0530

    Fixed Base Path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio/', // Replace 'portfolio' with your exact repo name
+  base: '/', // Replace 'portfolio' with your exact repo name
   plugins: [react()],
 })
